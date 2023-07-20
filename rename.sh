a13106005a0fb460d572aef71fb72cefa3331a99
daudix-UFO <ddaudix@gmail.com>
2023-07-20 19:12:02 +0300

    feat: Improved rename script (shoutout to ChatGPT)

--- rename.sh
+++ rename.sh
@@ -1,46 +1,52 @@
 #!/usr/bin/env bash
 
-# Check if a directory argument is provided
-if [[ -n "$1" ]]; then
-    # Use the specified directory
-    target_directory="$1"
-else
-    # Use the current directory if no argument is provided
-    target_directory="."
-fi
+# Made by ChatGPT and countless tweak requests
+# Script for renaming files to be web-friendly (kebab-case without special characters)
 
-# Function to rename blog files
-rename_blog_files() {
-    # Loop through each file in the specified directory
-    for file in "$target_directory"/*; do
-        # Check if the file is a regular file (to avoid directories)
-        if [[ -f "$file" ]]; then
-            # Get the filename without the directory path
-            filename=$(basename "$file")
-            
-            # Separate filename and extension
-            filename_no_ext="${filename%.*}"
-            extension="${filename##*.}"
-            
-            # Convert the filename to lowercase
-            new_filename=$(echo "$filename_no_ext" | tr '[:upper:]' '[:lower:]')
-            # Replace spaces with hyphens
-            new_filename=${new_filename// /-}
-            # Remove all non-alphabet characters and hyphens
-            new_filename=$(echo "$new_filename" | tr -cd 'a-z0-9-')
-            
-            # Combine the new filename and the original extension
-            new_filename="$new_filename.$extension"
-            
-            # Check if the new filename is different from the current one
-            if [[ "$filename" != "$new_filename" ]]; then
-                # Rename the file
-                mv "$file" "$target_directory/$new_filename"
-                echo "Renamed '$file' to '$target_directory/$new_filename'"
-            fi
-        fi
-    done
+# Function to rename files
+rename_files() {
+  local dir="$1"
+  cd "$dir" || exit 1
+
+  for file in *; do
+    if [ -f "$file" ]; then
+      # Get the file extension (if any)
+      ext=""
+      if [[ $file == *.* ]]; then
+        ext=".${file##*.}"
+        new_name="${file%$ext}"
+      else
+        new_name="$file"
+      fi
+
+      # Normalize non-ASCII characters to their closest ASCII representation and remove consecutive dashes
+      new_name=$(echo "$new_name" | sed 's/[^[:alnum:]\.]/-/g; s/[[:space:]_]\+/-/g; s/-\+/-/g')
+
+      # Remove leading and trailing dashes
+      new_name="${new_name#-}"
+      new_name="${new_name%-}"
+
+      # Convert the filename to lowercase
+      new_name="${new_name,,}"
+
+      # Add back the file extension (if any)
+      new_name="$new_name$ext"
+
+      # Check if the new name is different from the old one
+      if [ "$file" != "$new_name" ]; then
+        mv -i "$file" "$new_name"
+        echo "Renamed: $file -> $new_name"
+      fi
+    fi
+  done
 }
 
-# Call the function to rename blog files
-rename_blog_files
+# Check if a directory argument is provided, otherwise use the current directory
+if [ $# -eq 1 ]; then
+  target_directory="$1"
+else
+  target_directory="."
+fi
+
+# Call the function with the specified directory
+rename_files "$target_directory"
