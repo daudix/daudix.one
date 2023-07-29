d48b0d27b23b780cbe4550ab7deef0d8d7d31a8e
daudix-UFO <ddaudix@gmail.com>
2023-07-29 18:45:23 +0300

    fix: Skeuo style being enabled for new users

--- script.js
+++ script.js
@@ -14,21 +14,27 @@ function toggleStyle() {
   localStorage.setItem("selectedStyle", nextStyle);
 }
 
-// On page load, apply the saved style preference (if any)
+// On page load, apply the saved style preference (if any) or set default to "style.css"
 document.addEventListener("DOMContentLoaded", function () {
   const selectedStyle = localStorage.getItem("selectedStyle");
-  if (selectedStyle) {
-    const originalStyle = document.getElementById("original-style");
-    const skeuoStyle = document.getElementById("skeuo-style");
-    originalStyle.disabled = selectedStyle === "style-skeuo.css";
-    skeuoStyle.disabled = selectedStyle === "style.css";
-  }
+
+  // Set the default style to "style.css" if no preference is saved in localStorage
+  const defaultStyle = "style.css";
+  const currentStyle = selectedStyle || defaultStyle;
+
+  // Get the reference to the existing stylesheets
+  const originalStyle = document.getElementById("original-style");
+  const skeuoStyle = document.getElementById("skeuo-style");
+
+  // Disable the current stylesheet and enable the new one
+  originalStyle.disabled = currentStyle === "style-skeuo.css";
+  skeuoStyle.disabled = currentStyle === "style.css";
 });
 
 // Keyboard shortcut to toggle styles
-document.addEventListener('keyup', function (event) {
+document.addEventListener("keyup", function (event) {
   // Check if the key pressed is the "T" key (case-insensitive)
-  if (event.key.toLowerCase() === 't') {
+  if (event.key.toLowerCase() === "t") {
     // Call the toggleStyle function
     toggleStyle();
   }
