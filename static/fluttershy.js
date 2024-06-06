a5d2dc69c89d1b5a35fb9d4f0a84b0df648f75fe
daudix <ddaudix@gmail.com>
2024-06-07 00:37:23 +0300

    Yeah no worky in iOS 15

--- static/fluttershy.js
+++ static/fluttershy.js
@@ -1,12 +1,6 @@
 const viewportWidth = window.innerWidth;
 const fluttershy = document.getElementById("fluttershy");
 
-// Fallback for older browsers (burn in hell Apple!)
-function updateVwVariable() {
-	let vw = window.innerWidth || document.documentElement.clientWidth;
-	document.documentElement.style.setProperty("--vw", `-${vw}px`);
-}
-
 function handleFluttershyClick() {
 	fluttershy.classList.add("flying");
 	fluttershy.addEventListener("animationend", function () {
@@ -14,6 +8,4 @@ function handleFluttershyClick() {
 	});
 }
 
-updateVwVariable();
-window.addEventListener("resize", updateVwVariable);
 document.getElementById("shy").addEventListener("click", handleFluttershyClick);
