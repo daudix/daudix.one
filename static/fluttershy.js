ca09142e2be8567698ec3044149cdaa8164ec572
daudix <ddaudix@gmail.com>
2024-06-06 22:30:01 +0300

    Attempt to fix the pony easter egg (still a mess but slightly better)

--- static/fluttershy.js
+++ static/fluttershy.js
@@ -1,32 +1,9 @@
-function insertFluttershy() {
-	const img = document.createElement("img");
-	img.id = "fluttershy";
-	img.className = "transparent no-hover";
-	img.src = "fluttershy.gif";
-	document.body.appendChild(img);
-	updateFluttershyPosition();
-}
-
 function handleFluttershyClick() {
-	const img = document.getElementById("fluttershy");
-	if (img) {
-		img.classList.add("fly");
-		setTimeout(() => {
-			img.classList.remove("fly");
-		}, 2000);
-	}
+	const fluttershy = document.getElementById("fluttershy");
+	fluttershy.classList.add("flying");
+	fluttershy.addEventListener("animationend", function () {
+		fluttershy.classList.remove("flying");
+	});
 }
 
-function updateFluttershyPosition() {
-	const img = document.getElementById("fluttershy");
-	const siteNav = document.getElementById("site-nav");
-	if (img && siteNav) {
-		const navRect = siteNav.getBoundingClientRect();
-		img.style.transform = `translateY(${navRect.top}px)`;
-	}
-}
-
-insertFluttershy();
 document.getElementById("shy").addEventListener("click", handleFluttershyClick);
-window.addEventListener("scroll", updateFluttershyPosition);
-window.addEventListener("resize", updateFluttershyPosition);
