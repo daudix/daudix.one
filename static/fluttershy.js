fecdbe7fd046972d1bddc36dd3b436174e33a44d
daudix <ddaudix@gmail.com>
2024-06-06 23:20:22 +0300

    Fallback for older browsers

--- static/fluttershy.js
+++ static/fluttershy.js
@@ -1,9 +1,19 @@
+const viewportWidth = window.innerWidth;
+const fluttershy = document.getElementById("fluttershy");
+
+// Fallback for older browsers (burn in hell Apple!)
+function updateVwVariable() {
+	let vw = window.innerWidth || document.documentElement.clientWidth;
+	document.documentElement.style.setProperty("--vw", `-${vw}px`);
+}
+
 function handleFluttershyClick() {
-	const fluttershy = document.getElementById("fluttershy");
 	fluttershy.classList.add("flying");
 	fluttershy.addEventListener("animationend", function () {
 		fluttershy.classList.remove("flying");
 	});
 }
 
+updateVwVariable();
+window.addEventListener("resize", updateVwVariable);
 document.getElementById("shy").addEventListener("click", handleFluttershyClick);
